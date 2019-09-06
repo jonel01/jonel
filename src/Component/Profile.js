a7825381c928fc5d6075b27494f3900d72158c4e
Jonel <jonel01sandeigo@gmail.com>
2019-09-06 15:24:47 +0800

    typed js

--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Typed from 'react-typed';
 
 export const Profile = () => {
     return (
-        <h1>This is my Profile!</h1>
+        <Typed 
+            strings={['Hello I am Jonel San Diego',
+                'I am an Associate Developer',]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop />
     );
 }
