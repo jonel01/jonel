4c7df8dac09cb4c230d13fd7e86235f3d12b7185
Jonel <jonel01sandeigo@gmail.com>
2019-09-16 18:18:55 +0800

    complete profile

--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -1,15 +1,44 @@
 import React, { PureComponent } from 'react';
 import profileImage from '../assets/image/profile.jpg';
+import unknownImage from '../assets/image/unknown.jpeg';
+import unknownData from '../assets/image/unknowndata.png';
 import Typed from 'react-typed';
-import { MDBContainer, MDBBtn, MDBRow, MDBCol, MDBBadge } from "mdbreact";
+import { MDBContainer, MDBBtn, MDBRow, MDBCol, MDBBadge, MDBAnimation } from "mdbreact";
 import { styles } from '../ComponentStyle/Profile.css';
 
-class Profile extends PureComponent {
+import reactJS from '../assets/image/React-logo.png';
+import stackOverFlow from '../assets/image/stackoverflow.png';
+import youTube from '../assets/image/youtube.jpg';
+import npmJS from '../assets/image/npmjs.png';
+
+export default class Profile extends PureComponent {
     state = {
         visible: false,
-        boolean: false
+        boolean: false,
+        photo: 0,
+        images:[
+            { image: reactJS },
+            { image: stackOverFlow },
+            { image: youTube },
+            { image: npmJS }
+        ],
+        time: new Date()
     };
+
+    componentDidMount = () => {
+        this.timerID = setInterval(() => this.newDate(), 1000);
+    }
     
+    componentWillUnmount = () => {
+        clearInterval(this.timerID);
+    }
+
+    newDate = () => {
+        this.setState({
+          time: new Date()
+        });
+    }
+
     btnTypedHandlerHide = (e) => {
         e.preventDefault();
         this.setState({
@@ -28,6 +57,7 @@ class Profile extends PureComponent {
             boolean: false,
             loop: false
         });
+        
     }
     btnShowProfileHandler = (e) => {
         e.preventDefault();
@@ -35,23 +65,45 @@ class Profile extends PureComponent {
             boolean: true,
         });
     }
+    handleImageReveal = (image) => {
+        this.setState({
+            photo: image
+        })
+    }
+
 render(){
-    const { visible, boolean } = this.state
+    const { visible, boolean, images, photo, time } = this.state
 
     return (
         <MDBContainer style={styles.container}>
             <MDBRow md='12'>
                 <MDBCol md='6'>
-                    <img 
-                        src={profileImage} 
-                        alt="pic" 
-                        style={styles.profileImg}
-                    />
+                    {
+                        photo === 0 ?
+                    <div>
+                        <MDBAnimation type="bounce" delay="1s">
+                            <MDBBadge style={styles.revealPhoto} onClick={() => this.handleImageReveal(1)}>View Photo</MDBBadge>
+                        </MDBAnimation>
+                        <img 
+                            src={unknownImage} 
+                            alt="pic" 
+                            style={styles.profileImg}
+                        />
+                    </div>:
+                    <div>
+                        <MDBBadge style={styles.revealPhoto} onClick={() => this.handleImageReveal(0)}>Hide Photo</MDBBadge>
+                            <img 
+                                src={profileImage} 
+                                alt="pic" 
+                                style={styles.profileImg}
+                            />
+                    </div>
+                    }
                 </MDBCol>
                 <MDBCol md='6'>
                     {
                         boolean ? 
-                    <MDBContainer md="12">     
+                    <MDBContainer md="12">  
                         <MDBRow md="12">
                             <MDBBadge
                             onClick={this.btnHideProfileHandler}
@@ -60,15 +112,18 @@ render(){
                                 Hide Profile
                             </MDBBadge>
                         </MDBRow>  
-                        <MDBRow md="12" style={styles.pos}>       
-                                <Typed
-                                    strings={['TO OBTAIN A POSITION WHERE MY SKILLS, ABILITIES AND EDUCATION CAN BE UTILIZED AND EXPANDED ALSO TO HAVE A BETTER EXPERIENCE AND HAVE CONFIDENCE IN DEALING WITH OTHER PEOPLE,']}
-                                    typeSpeed={10}
-                                    backSpeed={20}
-                                    backDelay={6000}
-                                    loop={true}
-                                    />
-                        </MDBRow>               
+                            <MDBRow md="12" style={styles.pos}>
+                                <MDBAnimation type="fadeInRight" delay="1s" style={styles.stringStyle}>    
+                                    <Typed
+                                        strings={['TO OBTAIN A POSITION WHERE MY SKILLS, ABILITIES AND EDUCATION CAN BE UTILIZED AND EXPANDED ALSO TO HAVE A BETTER EXPERIENCE AND HAVE CONFIDENCE IN DEALING WITH OTHER PEOPLE.']}
+                                        typeSpeed={10}
+                                        backSpeed={20}
+                                        backDelay={6000}
+                                        loop={true}
+                                        startDelay={2000}
+                                        />
+                                </MDBAnimation>
+                            </MDBRow>
                     </MDBContainer>
                     :
                     <MDBContainer md="12">
@@ -80,6 +135,11 @@ render(){
                                 View Profile
                             </MDBBadge>
                         </MDBRow>
+                        <MDBRow md="12">
+                            <MDBAnimation type="bounceInDown" delay="1s">
+                                <img src={ unknownData } style={styles.unknownDataStyle}></img>
+                            </MDBAnimation>
+                        </MDBRow>
                     </MDBContainer>
                     }
                 </MDBCol>
@@ -87,7 +147,7 @@ render(){
 
         {
             visible ?
-        <MDBRow>
+        <MDBRow style={styles.btnViewInfo}>
             <MDBBtn 
                 type="submit" 
                 color="info" 
@@ -98,7 +158,8 @@ render(){
             <MDBCol md='12' style={styles.info}>         
                 <Typed style={styles.infoColor}
                 strings={['Hello I am Jonel M. San Diego',
-                    'I am an Associate Developer',]}
+                    'I am an Associate Developer',
+                    `Experienced in Front-End ReactJS Developer`]}
                     typeSpeed={40}
                     backSpeed={50}
                     backDelay={2000}
@@ -106,7 +167,7 @@ render(){
             </MDBCol>
         </MDBRow>
         :
-        <MDBRow>
+        <MDBRow style={styles.btnViewInfo}>
             <MDBBtn 
                 type="submit" 
                 color="info" 
@@ -117,10 +178,30 @@ render(){
         </MDBRow>
         }
         
-        
+        <MDBRow>
+                <MDBCol md='12' style={styles.timerContainer}>
+                    <MDBAnimation type="fadeInDown" delay="2s">
+                        <h2 style={styles.timer}>Time: {time.toLocaleTimeString()}</h2>
+                    </MDBAnimation>
+                </MDBCol>
+        </MDBRow>
+
+        <MDBRow>
+            <MDBCol md='12' style={styles.imageContainer}>
+                {
+                    <MDBAnimation type="fadeInUp" delay="2s">
+                    <h2 style={styles.imageText}>My Most Visited Websites</h2>
+                        {
+                    images.map(imageItems =>(
+                        <img src={imageItems.image} style={styles.imgStyle}></img>
+                    ))
+                        }
+                    </MDBAnimation>
+                }
+            </MDBCol>
+        </MDBRow>
+
         </MDBContainer>
     );
 }
 }
-
-export default Profile
--- src/ComponentStyle/Profile.css.js
+++ src/ComponentStyle/Profile.css.js
@@ -3,7 +3,27 @@ export const styles = {
         marginLeft: '150px'
     },
     container: {
-        marginTop: '120px'
+        marginTop: '120px',
+    },
+    imageContainer: {
+        backgroundColor: 'rgb(29, 5, 5)',
+        opacity: '0.7',
+        marginTop: '0px',
+        marginBottom: '50px',
+        marginLeft: '100px'
+    },
+    imgStyle: {
+        height: '150px',
+        paddingLeft: '20px',
+        paddingTop: '0px',
+        marginBottom: '20px',
+        width: '250px'
+    },
+    imageText: {
+        color: 'white',
+        fontWeight: 'bold',
+        marginLeft: '350px',
+        marginTop: '20px'
     },
     info: {
         marginLeft: '-80px',
@@ -15,19 +35,53 @@ export const styles = {
         fontSize: '20px',
         fontWeight: 'bold',
         padding: '5px',
-        opacity: '0.3'
     },
     pos: {
         backgroundColor: 'black',
         color:'white',
         padding: '6px',
-        marginTop: '20px',
-        marginRight: '100px',
-        opacity: '0.5'
+        marginTop: '10px',
+        marginLeft: '-230px',
+        opacity: '0.8',
+        width: '800px'
     },
     profileImg: {
         height: '150px',
         marginLeft: '-10px',
         width: '150px',
     },
+    revealPhoto: {
+        fontSize: '8',
+        position: 'absolute',
+        marginTop: '-25px',
+        marginLeft: '30px'
+    },
+    stringStyle: {
+        marginBottom: '15px',
+        marginLeft: '40px',
+        marginRight: '40px',
+        marginTop: '15px',
+        width: '1000px'
+    },
+    timer: {
+        color: 'white',
+        fontSize: '60px',
+        fontWeight: 'bold',
+        marginTop: '5px',
+        marginBottom: '5px',
+        marginLeft: '260px',
+        padding: '10px'
+    },
+    timerContainer: {
+        backgroundColor: 'rgb(16, 10, 25)',
+        marginTop: '20px',
+        marginLeft: '100px',
+        opacity: '0.6',
+    },
+    unknownDataStyle: {
+        height: '120px',
+        marginLeft: '136px',
+        marginTop: '15px',
+        width: '100px'
+    }
 }
